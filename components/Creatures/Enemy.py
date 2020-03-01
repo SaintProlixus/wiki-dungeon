fbe72eca770bb59763f7e1d47167998c04ea81b0
Nate <nrstone@uab.edu>
2020-03-01 01:12:58 -0600

    Adjust enemy state

--- components/Creatures/Enemy.py
+++ components/Creatures/Enemy.py
@@ -2,11 +2,11 @@ from Combatant import Combatant
 
 
 class Enemy(Combatant):
-    def __init__(self, e_class, *args, **kwargs):
+    def __init__(self, e_class=None, *args, **kwargs):
+        super(Enemy, self).__init__(*args, **kwargs)
         self.e_class = e_class
         self.loot = None
         self.exp = None
-        super(Enemy, self).__init__(*args, **kwargs)
 
     def notice(self, player):
         pass
